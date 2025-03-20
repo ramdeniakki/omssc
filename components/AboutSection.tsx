51dac7c84eefffdf5a33731cabfc7b9ba10b7b44
Ramdeniakki <ramdeniakki35@gmail.com>
2025-03-20 12:18:13 +0530

    modified Aboutsection

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,5 +1,4 @@
 import { CheckCircle } from "lucide-react"
-import Image from "next/image"
 
 export default function AboutSection() {
   return (
@@ -31,13 +30,16 @@ export default function AboutSection() {
             ))}
           </ul>
         </div>
-        <div className="relative h-[400px] rounded-lg overflow-hidden">
-          <Image
-            src="/placeholder.svg?height=800&width=600"
-            alt="Bicycle shop interior"
-            fill
-            className="object-cover"
-          />
+        <div className="relative h-[450px] rounded-lg overflow-hidden">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3755.4160222768323!2d79.1678674752222!3d19.737471681608014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd2c5c6ea6fdff9%3A0x86ff940ffae429b6!2sOM%20SRI%20SAI%20CYCLE%20STORES!5e0!3m2!1sen!2sin!4v1742452758630!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </section>
